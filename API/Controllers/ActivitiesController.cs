8306a0412c017cefd585f565dd92326534a56e2f
sheikhmashfiqrahman <rmashfiq1@gmail.com>
2023-01-05 07:22:29 +1100

    Api Controller Added for get Request

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
@@ -9,8 +9,23 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController 
     {   
         private readonly DataContext _context;
+        
         public ActivitiesController (DataContext context){
-            _context=context;
+            _context = context;
+         
+        }
+
+        [HttpGet] // api/activities for get requests
+
+        public async Task<ActionResult<List<Activity>>> getAtctivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // it will use id for getting specific activity // api/activities/id
+
+        public async Task <ActionResult<Activity>> GetActivity (Guid id){
+            return await _context.Activities.FindAsync(id);
         }
         
     }
