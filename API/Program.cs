d875dc2bfd1772e63dde00db314b821ab3a94da8
sheikhmashfiqrahman <rmashfiq1@gmail.com>
2023-01-09 11:17:03 +1100

    added cors policy in the backend

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt=>{
+
+    opt.AddPolicy("CorsPolicy", policy =>{
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +32,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// order needs to be maintained as this is a pipeline
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
